9f4a8f5ae0a62c2e167fc896657587aa26dd03d9
tmdoug12 <125109246+tiuna12@users.noreply.github.com>
2023-02-18 02:03:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+def main():
+  intro()
+  try:
+    print('To Start')
+    miles= int(input ("Enter The Number Of Miles Driven:  "))
+    print()
+    miles_to_kilometers(miles)
+    print()
+  except(ValueError):
+    print("      Opps!! Try Again By Entering Only Numbers")
+    print()
+    main() 
+ 
+def intro():
+  print( "Welcome To My Program")
+  print("This Program Converts Measurement")
+  print("In Miles To Kilometers")
+  print()
+
+def miles_to_kilometers(miles):
+  kilometers = miles * 1.60934
+  print(f"The Total Of {miles} Miles Driven is equal to {kilometers} Kilometers")
+  print()
+  answer()
+  
+def answer():
+  answer= str(input ('Will You Like To Do Another Conversion? Yes Or No: '))
+  if answer== "no":
+    print()
+    print("Thank You For Using My Program GoodBye")
+  elif answer== "yes":
+    print()
+    main()
+
+  
+main()
